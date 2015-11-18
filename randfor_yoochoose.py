96cd5e8871d87e05da9e39fc95ba25e1df536665
Jeremy Silver <jeremander@Jeremys-MacBook-Pro.local>
2015-11-18 11:43:52 -0500

    Wrote new Yoochoose-specific random forest script.

--- randfor_yoochoose.py
+++ randfor_yoochoose.py
@@ -1,5 +1,7 @@
 # Given a labeled data set (arbitrary features, binary response) and a filename with list of features to evaluate, constructs random forest model and compares the features
 
+# Trains and tests random forest on the full Yoochoose data set. Saves off the model, test set probabilities, and report of classification statistics
+
 import optparse
 import pandas as pd
 import numpy as np
@@ -8,48 +10,32 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 
+
 def main():
     p = optparse.OptionParser()
-    p.add_option('--model', '-m', default = 'model', type = str, help = 'model filename prefix')
     p.add_option('--load', '-L', default = False, action = 'store_true', help = 'load model from file')
     p.add_option('--features', '-f', default = 'features.txt', type = str, help = 'feature filename')
-    p.add_option('--data', '-d', default = 'data.csv', type = str, help = 'marked data filename')
     p.add_option('--verbose', '-v', default = False, action = 'store_true', help = 'verbosity flag')
     p.add_option('--thresh', '-T', default = 0.5, type = float, help = 'probability threshold to classify True')
     p.add_option('--n_estimators', '-n', default = 100, type = int, help = 'number of random forest estimators')
-    p.add_option('--test_fraction', '-t', default = 0.25, type = float, help = 'fraction of data to use for testing')
     p.add_option('--seed', '-s', default = None, type = int, help = 'random seed')
     p.add_option('--jobs', '-j', default = -1, type = int, help = 'number of jobs (-1 if maximum)')
     opts, args = p.parse_args()
 
-    model_filename = opts.model + '%s.pickle' % ('' if opts.seed is None else str(opts.seed))
+    model_filename = 'model%s.pickle' % ('' if opts.seed is None else str(opts.seed))
 
     np.random.seed(opts.seed)
 
     if opts.verbose:
-        print("\nReading marked data from %s..." % opts.data)
+        print("\nReading data set...")
 
-    # establish data frame
-    df = pd.read_csv(opts.data)
-    n_lines = len(df)
-
-    # choose test set as random test_fraction of data, leaving the remainder for training
-    n_test = int(opts.test_fraction * n_lines)
-    if opts.verbose:
-        print("Read %d lines of data -> %d lines (training), %d lines (test)" % (n_lines, n_lines - n_test, n_test))
-    test_subset = np.random.permutation(range(n_lines))[:n_test]
-    is_train = np.ones(n_lines, dtype = bool)
-    for i in test_subset:
-        is_train[i] = False
-
-    # establish training and test sets
-    train, test = df[is_train], df[~is_train]
+    train = pd.read_csv('data/yoochoose-training_session_features.csv').append(pd.read_csv('data/yoochoose-dev_session_features.csv'))
+    test = pd.read_csv('data/yoochoose-test_session_features.csv')
 
     if opts.load:
         rfc = pickle.load(open(model_filename, 'rb'))
         if opts.verbose:
             print("\nLoaded model from '%s'.\n" % model_filename)
-
     else:
         # set the random forest instance
         rfc = RandomForestClassifier(n_estimators = opts.n_estimators, n_jobs = opts.jobs)
@@ -89,33 +75,26 @@ def main():
 
     # make predictions on the test data
     probs = rfc.predict_proba(test[rfc.input_features])[:, 1]
+    probs_series = pd.Series(probs)
+    probs_series.to_csv('test_probs%s' % ('' if opts.seed is None else str(opts.seed)))
     test_preds = (probs >= opts.thresh)
     conf_df = pd.crosstab(test[rfc.output_feature], test_preds, rownames = ['actual'], colnames = ['predicted'])
     conf_mat = np.asarray(conf_df)
     class_report = classification_report(test[rfc.output_feature], test_preds)
-    print("\nConfusion Matrix")
-    print(conf_df)
-    print("\nClassification Report")
-    print(class_report)
+    s = "\nConfusion Matrix\n"
+    s += conf_df + '\n'
+    s += "\nClassification Report\n"
+    s += class_report + '\n'
     accuracy = (conf_mat[0, 0] + conf_mat[1, 1]) / float(np.sum(conf_mat))
-    print("Accuracy = %.3f%%" % (100. * accuracy))
-    print("\nFeature Importances")
+    s += "Accuracy = %.3f%%\n" % (100. * accuracy)
+    s += "\nFeature Importances\n"
     triples = [(i, rfc.input_features[i], rfc.feature_importances_[i]) for i in range(num_features)]
     triples.sort(key = lambda pair : pair[2], reverse = True)
     indices, features, importances = zip(*triples)
     for i in range(num_features):
-        print("%17s   %3d.%03d%%" % (features[i], int(100. * importances[i]), round(1000 * (100. * importances[i] - int(100. * importances[i])))))
-    stds = np.std([tree.feature_importances_ for tree in rfc.estimators_], axis = 0)
-    plt.figure()
-    plt.title("Feature importances")
-    plt.bar(range(X.shape[1]), importances, color = 'r', yerr = [stds[i] for i in indices], align = 'center')
-    plt.xticks(range(X.shape[1]), features, rotation = 'vertical')
-    plt.xlim([-1, X.shape[1]])
-    fig = plt.gcf()
-    fig.subplots_adjust(bottom = 0.25)
-    plt.savefig('feature_importances%s.png' % ('' if opts.seed is None else str(opts.seed)))
-    #plt.show()
-
+        s += "%17s   %3d.%03d%%\n" % (features[i], int(100. * importances[i]), round(1000 * (100. * importances[i] - int(100. * importances[i]))))
+    with open('test_report%s' % ('' if opts.seed is None else str(opts.seed)), 'w') as f:
+        f.write(s)
 
 if __name__ == "__main__":
-    main()
+    main() 
